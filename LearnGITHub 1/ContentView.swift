761b6272a0a85ca0ed2e91e1e7a5444e24abf571
Ari Nugrahanto <me@arinugrahanto.com>
2020-04-24 11:21:02 +0700

    init

---
+++ LearnGITHub 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnGITHub 1
+//
+//  Created by Ari Nugrahanto on 24/04/20.
+//  Copyright © 2020 ADA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Tafa!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
